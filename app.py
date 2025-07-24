d1f67bfda8f75f9f4ba4ce625a928ccfd7cffa75
tnkaun <tnkaun@gmail.com>
2025-07-25 01:07:29 +0800

    修改app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello 老哥，這是用python flask寫的!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
+
 
