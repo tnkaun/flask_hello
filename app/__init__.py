3097f62500f3106ff14c3ff73fc4d37608f3f73c
tnkaun <tnkaun@gmail.com>
2025-07-25 23:53:42 +0800

    fix:db

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,17 @@ from app.routes.about import about_bp
 from app.routes.contact import contact_bp
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from app.extensions import db
 
-db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
+    
     app.config.from_object(Config)
-
+    db.init_app(app)
     app.register_blueprint(home_bp)
     app.register_blueprint(about_bp)
     app.register_blueprint(contact_bp)
 
-    with app.app_context():
-        db.create_all()
+    
     return app
--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
-from app import db
+from app.extensions import db
+
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    content = db.Column(db.Text)
+    content = db.Column(db.Text, nullable=False)
