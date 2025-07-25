0d19d5ac298fca545333b285b8fc03c5da76457f
tnkaun <tnkaun@gmail.com>
2025-07-25 17:16:19 +0800

    fix:struct

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from app.routes.home import home_bp
+from app.routes.about import about_bp
+from app.routes.contact import contact_bp
 
 def create_app():
     app = Flask(__name__)
-    
-    from app.routes.main_routes import main_bp
-    app.register_blueprint(main_bp)
-
+    app.register_blueprint(home_bp)
+    app.register_blueprint(about_bp)
+    app.register_blueprint(contact_bp)
     return app
