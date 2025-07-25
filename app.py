2ffa3bb1b83a7818cdb98fb383512187c15de868
tnkaun <tnkaun@gmail.com>
2025-07-25 23:34:15 +0800

    fix:db

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from app import create_app
-import os
+from app import create_app, db
 
 app = create_app()
 
-if __name__ == '__main__':
+with app.app_context():
+    try:
+        # 試著查詢資料庫
+        db.session.execute('SELECT 1')
+        print("✅ 資料庫連線成功！")
+    except Exception as e:
+        print("❌ 資料庫連線失敗：", e)
+
+if __name__ == "__main__":
+    
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host="0.0.0.0", port=port)
