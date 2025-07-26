da660b3b7736a4df5ce3b3b4e12cb7539f09e7c7
tnkaun <tnkaun@gmail.com>
2025-07-26 09:57:55 +0800

    initial commit

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class Message(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
