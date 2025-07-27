6e2cf7cd28b5dfb1cc256ecafbf8a24f3cf1d31f
tnkaun <tnkaun@gmail.com>
2025-07-28 00:02:01 +0800

    fix:message

--- app/routes/board.py
+++ app/routes/board.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template,request, jsonify
+from flask import Blueprint, render_template,request, jsonify,redirect,url_for
 from app.models import Message
 from app.extensions import db
 
@@ -15,3 +15,10 @@ def board():
 
     all_msgs = Message.query.all()
     return render_template("board.html", messages=all_msgs)
+
+@board_bp.route("/board/delete/<int:msg_id>", methods=["POST"])
+def delete_msg(msg_id):
+    msg = Message.query.get_or_404(msg_id)
+    db.session.delete(msg)
+    db.session.commit()
+    return redirect(url_for("board.board"))
