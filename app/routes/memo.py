1bce142f9d722b2287da01bb14b2c722d4df440f
tnkaun <tnkaun@gmail.com>
2025-07-27 23:47:14 +0800

    delet

--- app/routes/memo.py
+++ app/routes/memo.py
@@ -15,4 +15,12 @@ def memo():
     all_memos = Memo.query.all()
     return render_template("memo.html", memos=all_memos)
 
+@memo_bp.route("/memo/delete/<int:memo_id>", methods=["GET"])
+def delete_memo(memo_id):
+    memo = Memo.query.get_or_404(memo_id)
+    db.session.delete(memo)
+    db.session.commit()
+    return redirect(url_for("memo.memo"))
+
+
 
